287e24202807714aa7ac2cd527b1f7568da9dc97
Hans Mattin-Lassei <hans.mattin-lassei@avanoria.se>
2026-03-20 12:12:02 +0100

    Member Services added

---
+++ Infrastructure/Persistence/Repositories/MemberRepository.cs
@@ -0,0 +1,36 @@
+﻿using Application.Abstractions.Persistence;
+using Domain.Entities;
+using Infrastructure.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Infrastructure.Persistence.Repositories;
+
+public class MemberRepository(PersistenceContext context) : IMemberRepository
+{
+    public async Task AddAsync(Member member, CancellationToken ct = default)
+    {
+        await context.AddAsync(member, ct);
+        await context.SaveChangesAsync(ct);
+    }
+
+    public async Task UpdateAsync(Member member, CancellationToken ct = default)
+    {
+        context.Update(member);
+        await context.SaveChangesAsync(ct);
+    }
+
+    public async Task RemoveAsync(Member member, CancellationToken ct = default)
+    {
+        context.Remove(member);
+        await context.SaveChangesAsync(ct);
+    }
+
+    public async Task<Member?> GetAsync(Expression<Func<Member, bool>> expression, CancellationToken ct = default)
+    {
+        var member = await context.Members
+            .FirstOrDefaultAsync(expression, ct);
+
+        return member;
+    }
+}
